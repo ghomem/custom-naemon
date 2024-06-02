db7000c3d0923d48317a89670ce9255fac80be91
PrinceSalatyell <salatyelll14@gmail.com>
2024-06-02 22:07:56 +0100

    very last one

--- utils/install_naemon.sh
+++ utils/install_naemon.sh
@@ -45,6 +45,7 @@ sed -i 's|^Listen 80|Listen 8080|' /etc/apache2/ports.conf
 sed -i "s|^\s*#ServerName\s.*|        ServerName ${FQDN}|" /etc/apache2/sites-available/000-default.conf
 sed -i 's|^cookie_auth_restricted_url        = http://localhost/thruk/cgi-bin/restricted.cgi|cookie_auth_restricted_url        = http://localhost:8080/thruk/cgi-bin/restricted.cgi|' /etc/thruk/thruk.conf
 sed -i 's|^STARTURL="http://localhost/thruk/cgi-bin/remote.cgi?startup"|STARTURL="http://localhost:8080/thruk/cgi-bin/remote.cgi?startup"|' /etc/init.d/thruk
+sed -i 's|^::1 localhost ip6-localhost ip6-loopback|::1 ip6-localhost ip6-loopback|' /etc/hosts
 
 systemctl daemon-reload
 
